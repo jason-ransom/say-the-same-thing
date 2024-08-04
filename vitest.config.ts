4016acbbe19cb8047041cfe758b28947bd4f7067
Jason Ransom <jasonlawrenceransom@gmail.com>
2024-08-03 21:34:50 -0400

    css fixes for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,5 +11,10 @@ export default defineConfig({
         alias: {
             '@': '/'
         }
-    }
+    },
+    css: {
+        modules: {
+            scopeBehaviour: 'local',
+        },
+    },
 })
